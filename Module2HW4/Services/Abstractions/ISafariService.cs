1b0c0cc6e93606495822b2d39c86fcb1b436069b
Dima <yadima2001@gmail.com>
2021-11-21 13:12:42 +0200

    add safari service and abstraction

---
+++ Module2HW4/Services/Abstractions/ISafariService.cs
@@ -0,0 +1,13 @@
+﻿using Module2HW4.Models;
+
+namespace Module2HW4.Services.Abstractions
+{
+    public interface ISafariService
+    {
+        public void Fill();
+        public Animal[] GetAnimals();
+        public void SortByWeight();
+        public void Clear();
+        public Animal FindByWeight();
+    }
+}
---
+++ Module2HW4/Services/SafariService.cs
@@ -0,0 +1,41 @@
+﻿using Module2HW4.Models;
+using Module2HW4.Providers.Abstractions;
+using Module2HW4.Services.Abstractions;
+
+namespace Module2HW4.Services
+{
+    public class SafariService : ISafariService
+    {
+        private readonly IAnimalProvider _animalProvider;
+        public SafariService(IAnimalProvider animalProvider)
+        {
+            _animalProvider = animalProvider;
+        }
+
+        private Animal[] Animals { get; set; }
+        public void Clear()
+        {
+           Animals = new Animal[0];
+        }
+
+        public void Fill()
+        {
+            Animals = _animalProvider.GetAnimals();
+        }
+
+        public Animal FindByWeight()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Animal[] GetAnimals()
+        {
+            return Animals;
+        }
+
+        public void SortByWeight()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
