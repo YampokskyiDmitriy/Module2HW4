ddd646feb540645f3bfc05de65c29511528978c8
Dima <yadima2001@gmail.com>
2021-11-21 13:28:39 +0200

    add animal array extension

---
+++ Module2HW4/Helpers/AnimalArrayExtension.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Module2HW4.Models;
+
+namespace Module2HW4.Helpers
+{
+    public static class AnimalArrayExtension
+    {
+        public static Animal[] FindByWeight(this Animal[] array, double weight)
+        {
+            var result = new Animal[0];
+            foreach (var animal in array)
+            {
+                if (animal.Weight == weight)
+                {
+                    Array.Resize(ref result, result.Length + 1);
+                    result[result.Length - 1] = animal;
+                }
+            }
+
+            return result;
+        }
+    }
+}
