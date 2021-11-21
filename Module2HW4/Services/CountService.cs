a1e067be5120c876169bf2622259d7595f28b30e
Dima <yadima2001@gmail.com>
2021-11-21 16:21:45 +0200

    modify count service

--- Module2HW4/Services/CountService.cs
+++ Module2HW4/Services/CountService.cs
@@ -8,46 +8,19 @@ namespace Module2HW4.Services
         public int CountDifferentTypes(Animal[] animals)
         {
             var count = 0;
-            var quantityOfTypes = new int[8];
-            foreach (var animal in animals)
+            for (var i = 0; i < animals.Length; i++)
             {
-                if (animal.GetType().Name == "Scorpion")
+                var isDuplicate = false;
+                for (var j = 0; j < i; j++)
                 {
-                    quantityOfTypes[0]++;
+                    if (animals[i].GetType().Name == animals[j].GetType().Name)
+                    {
+                        isDuplicate = true;
+                        break;
+                    }
                 }
-                else if (animal.GetType().Name == "Spider")
-                {
-                    quantityOfTypes[1]++;
-                }
-                else if (animal.GetType().Name == "Butterfly")
-                {
-                    quantityOfTypes[2]++;
-                }
-                else if (animal.GetType().Name == "Mantis")
-                {
-                    quantityOfTypes[3]++;
-                }
-                else if (animal.GetType().Name == "Elephant")
-                {
-                    quantityOfTypes[4]++;
-                }
-                else if (animal.GetType().Name == "Lion")
-                {
-                    quantityOfTypes[5]++;
-                }
-                else if (animal.GetType().Name == "Gecko")
-                {
-                    quantityOfTypes[6]++;
-                }
-                else if (animal.GetType().Name == "Turtle")
-                {
-                    quantityOfTypes[7]++;
-                }
-            }
 
-            foreach (var quant in quantityOfTypes)
-            {
-                if (quant != 0)
+                if (!isDuplicate)
                 {
                     count++;
                 }
