323620c4a98c44485676dbbfa5f6f4e2803b0c8f
Dima <yadima2001@gmail.com>
2021-11-21 14:09:43 +0200

    add starter

---
+++ Module2HW4/Starter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Module2HW4.Services.Abstractions;
+
+namespace Module2HW4
+{
+    public class Starter
+    {
+        private ISafariService _safariService;
+        public Starter(ISafariService safariService)
+        {
+            _safariService = safariService;
+        }
+
+        public void Run()
+        {
+            _safariService.Fill();
+            _safariService.SortByWeight();
+            var countOfTypes = _safariService.CountDifferentTypes();
+            var searchResult = _safariService.FindByWeight(0.33);
+            Console.WriteLine(countOfTypes);
+        }
+    }
+}
