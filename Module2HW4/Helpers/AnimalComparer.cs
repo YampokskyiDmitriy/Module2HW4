96e0075a1cffd68705a2fb8c6514e6a6bdd59256
Dima <yadima2001@gmail.com>
2021-11-21 13:19:00 +0200

    add animal comparer

---
+++ Module2HW4/Helpers/AnimalComparer.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using Module2HW4.Models;
+
+namespace Module2HW4.Helpers
+{
+    public class AnimalComparer : IComparer
+    {
+        public int Compare(object first, object second)
+        {
+            var firstAnimal = first as Animal;
+            var secondAnimal = second as Animal;
+            if (firstAnimal == null || secondAnimal == null)
+            {
+                return 0;
+            }
+
+            if (firstAnimal.Weight > secondAnimal.Weight)
+            {
+                return 1;
+            }
+            else if (firstAnimal.Weight < secondAnimal.Weight)
+            {
+                return -1;
+            }
+
+            return 0;
+        }
+    }
+}
